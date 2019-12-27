9a2d51b2374275aa8e757a4c75c91dd0feea03b0
john <zlmnhgd@163.com>
2019-12-27 19:34:42 +0800

    SqBiTree update

--- MyDataStructure/SqBiTree/SqBiTree.h
+++ MyDataStructure/SqBiTree/SqBiTree.h
@@ -13,22 +13,79 @@ TElemType Nil = 0; // 设整型以0为空
 
 typedef TElemType SqBiTree[MAX_TREE_SIZE]; // 0号单元存储根节点 
 
+typedef struct {
+	int level, order;
+} position;
+
 /*
  *  初始化二叉树
  *  每个节点均设置为空 
  */
 Status InitTree(SqBiTree *T);
 
+/*
+ *  销毁 
+ */
 Status DestroyTree(SqBiTree *T);
 
+/*
+ *  创建完全二叉树 
+ */ 
 Status CreateTree(SqBiTree *T);
 
+/*
+ *  清空二叉树 
+ */ 
 Status ClearTree(SqBiTree *T); 
 
+/*
+ * 二叉树判空 
+ */ 
 Status BiTreeEmpty(SqBiTree T);
 
+/*
+ * 二叉树的深度 
+ */ 
 int BiTreeDepth(SqBiTree T);
 
-TElemType Root(SqBiTree T); 
+/*
+ * 二叉树的根 
+ */ 
+Status Root(SqBiTree T, TElemType *e); 
+
+/*
+ *  返回处于位置e的节点的值 
+ */
+TElemType Value(SqBiTree T, position p);
+
+/*
+ *  给位于p位置处的节点赋新值value 
+ */
+Status Assign(SqBiTree T, position p, TElemType value);
+
+/*
+ *  返回节点e的双亲 
+ */
+TElemType Parent(SqBiTree T, TElemType e);
+
+/*
+ *  返回节点e的左孩子 
+ */ 
+TElemType LeftChild(SqBiTree T, TElemType e);
+
+/*
+ *  返回节点e的右孩子 
+ */
+TElemType RightChild(SqBiTree T, TElemType e);
+
+/*
+ *  返回e的左兄弟 
+ */
+TElemType LeftSibling(SqBiTree T, TElemType e);
+
+/*
+ *  返回e的右兄弟 
+ */
+TElemType RightSibling(SqBiTree T, TElemType e);
 
 #endif
