b237d934e1405954f47662caab7482b60186cfb6
john <zlmnhgd@163.com>
2020-01-01 17:51:41 +0800

    BiTree update

--- MyDataStructure/BiTree/BiTree.c
+++ MyDataStructure/BiTree/BiTree.c
@@ -157,25 +157,133 @@ TElemType Parent(BiTree T, TElemType e)
  * 初始条件： 二叉树T存在，e是T中某个结点
  * 操作结果：返回e的左孩子。若e无左孩子，则返回“空” 
  */
-TElemType LeftChild(BiTree T, TElemType e);
+TElemType LeftChild(BiTree T, TElemType e)
+{
+	if (!T) {
+		return ERROR;
+	} 
+	
+	if (T->data == e) {
+		if (T->lchild) {
+			return T->lchild->data;
+		}
+		else {
+			return NIL;
+		}
+	} 
+	else {
+		if (T->lchild) {
+			return LeftChild(T->lchild, e);
+		}
+		else if (T->rchild){
+			return LeftChild(T->rchild, e);
+		}
+		else {
+			return NIL;
+		}
+	}
+
+}
 
 /*
  * 初始条件： 二叉树T存在，e是T中某个结点
  * 操作结果：返回e的右孩子。若e无右孩子，则返回“空” 
  */
-TElemType RightChild(BiTree T, TElemType e);
+TElemType RightChild(BiTree T, TElemType e)
+{
+	if (!T) {
+		return ERROR;
+	} 
+	
+	if (T->data == e) {
+		if (T->rchild) {
+			return T->rchild->data;
+		}
+		else {
+			return NIL;
+		}
+	} 
+	else {
+		if (T->lchild) {
+			return RightChild(T->lchild, e);
+		}
+		else if (T->rchild){
+			return RightChild(T->rchild, e);
+		}
+		else {
+			return NIL;
+		}
+	}
+}
 
 /*
  * 初始条件：二叉树T存在，e是T中某个结点 
  * 操作结果：返回e的左兄弟。若e是T的左孩子或无左兄弟，则返回“空” 
  */
-TElemType LeftSibling(BiTree T, TElemType e);
+ /*
+TElemType LeftSibling(BiTree T, TElemType e)
+{
+	BiTree parent;
+	
+	if (!T) {
+		return NIL; 
+	}
+	
+	if (T->data == e) {
+		parent = Parent(T, e);
+		if (parent) {
+			if (parent->lchild) {
+				if (parent->lchild->data != e) {
+					return parent->lchild->data;
+				}
+			} 
+		} 
+	}
+	else {
+		if (T->lchild)
+			return LeftSibling(T->lchild, e);
+		else if(T->rchild){
+			return LeftSibling(T->rchild, e);
+		}
+	}
+	return NIL;
+}
+*/
+
 
 /*
  * 初始条件：二叉树T存在，e是T中某个结点
  * 操作结果：返回e的右兄弟。若e是T的右孩子或无右兄弟，则返回“空” 
  */
-TElemType RightSibling(BiTree T, TElemType e); 
+ /*
+TElemType RightSibling(BiTree T, TElemType e)
+{
+	BiTree parent;
+	
+	if (!T) {
+		return NIL; 
+	}
+	
+	if (T->data == e) {
+		parent = Parent(T, e);
+		if (parent) {
+			if (parent->rchild) {
+				if (parent->rchild->data != e) {
+					return parent->rchild->data;
+				}
+			} 
+		} 
+	}
+	else {
+		if (T->lchild)
+			return RightSibling(T->lchild, e);
+		else if(T->rchild){
+			return RightSibling(T->rchild, e);
+		}
+	}
+	return NIL;
+}
+*/
 
 /*
  * 初始条件：二叉树T存在，p指向T中某个结点，LR为0或1，非空二叉树c与T不相交且右子树为空 
@@ -193,19 +301,46 @@ Status DeleteChild(BiTree T, TElemType p, Status LR);
  * 采用二叉链表存储结构，Visit是对结点操作的应用函数
  * 先序遍历二叉树T，对每个结点调用函数Visit一次且仅一次  
  */ 
-Status PreOrderTraverse(BiTree T, void (Visit)(TElemType e));
+Status PreOrderTraverse(BiTree T, void (Visit)(TElemType e))
+{
+	if (!T) {
+		return NIL;
+	}
+	
+	Visit(T->data);
+	PreOrderTraverse(T->lchild, Visit);
+	PreOrderTraverse(T->rchild, Visit);
+}
 
 /*
  * 采用二叉链表存储结构，Visit是对结点操作的应用函数
  * 中序遍历二叉树T，对每个结点调用函数Visit一次且仅一次  
  */ 
-Status InOrderTraverse(BiTree T, void (Visit)(TElemType e));
+Status InOrderTraverse(BiTree T, void (Visit)(TElemType e))
+{
+	if (!T) {
+		return NIL;
+	}
+	
+	Visit(T->data);
+	InOrderTraverse(T->lchild, Visit);
+	InOrderTraverse(T->rchild, Visit);
+}
 
 /*
  * 采用二叉链表存储结构，Visit是对结点操作的应用函数
  * 后序遍历二叉树T，对每个结点调用函数Visit一次且仅一次  
  */ 
-Status PostOrderTraverse(BiTree T, void (Visit)(TElemType e));
+Status PostOrderTraverse(BiTree T, void (Visit)(TElemType e))
+{
+	if (!T) {
+		return NIL;
+	}
+	
+	Visit(T->data);
+	PostOrderTraverse(T->lchild, Visit);
+	PostOrderTraverse(T->rchild, Visit);
+}
 
 /*
  * 采用二叉链表存储结构，Visit是对结点操作的应用函数
--- MyDataStructure/BiTree/main.c
+++ MyDataStructure/BiTree/main.c
@@ -14,5 +14,7 @@ int main(int argc, char *argv[]) {
 	*/
 	CreateBiTree(&T);
 	
+	PreOrderTraverse(T, Visit);
+	
 	return 0;
 }
